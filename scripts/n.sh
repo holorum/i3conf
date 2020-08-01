770a3fe92d4ae2b17c9d28d8301dd9366c1cf7d5
holorum <atc927@gmail.com>
2020-08-01 08:32:49 +0200

    added scripts

---
+++ scripts/n.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+nitrogen --head=0 --set-auto /home/holorum/Downloads/azur-lane-long-island-chain-anime-games-anime-19815.jpg
+nitrogen --head=1 --set-auto /home/holorum/Downloads/Senjougahara-Hitagi-Monogatari-Series-No-waifu-no-laifu-anime-girls-manga-winter-1522037-wallhere.com.png
---
+++ scripts/wait.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sleep $1
+mpv -- /home/holorum/Tore.flac
