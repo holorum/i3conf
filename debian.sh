ca5272b5fa69291acce065bafaa5addb1306f329
holorum <atc927@gmail.com>
2022-02-20 12:47:07 +0100

    Added a script that sets up zsh on Debian.

---
+++ debian.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt install zsh neofetch bash-completion vim
+
+mkdir -p $HOME/.cache/zsh
+touch $HOME/.cache/zsh/history
+cp $HOME/i3conf/zsh/als.sh $HOME
+cp $HOME/i3conf/zsh/.zshrc.pre-oh-my-zsh $HOME/.zshrc
+chsh -s /usr/bin/zsh
+
+echo "Done!"
